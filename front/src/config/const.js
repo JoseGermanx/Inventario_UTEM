abc42b88b42f9b50ac0a22fc86c2861ab5533324
Jose Germán Martínez <jgermanmm@gmail.com>
2024-04-02 19:58:09 -0300

    Actualizar URL del backend en el archivo de constantes

--- front/src/config/const.js
+++ front/src/config/const.js
@@ -1,6 +1,6 @@
 
 export const variables = {
-    urlBaseBack: 'http://localhost:8080/api',
-    urlReportePdf: "http://localhost:8080/api/informe/generar-reporte-general-pdf",
-    urlReporteExcel: "http://localhost:8080/api/informe/generar-reporte-general-xls",
+    urlBaseBack: process.env.REACT_APP_URL_BACKEND,
+    urlReportePdf: `${process.env.REACT_APP_URL_BACKEND}/informe/generar-reporte-general-pdf`,
+    urlReporteExcel: `${process.env.REACT_APP_URL_BACKEND}/informe/generar-reporte-general-xls`,
 }
